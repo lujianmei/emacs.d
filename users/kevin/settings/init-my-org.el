3d82f79a6a1f844f508bcc74230a288297ac3b02
lujianmei <anysky130@163.com>
2015-10-26 00:57:00 +0800

    add taskjuggler support

--- users/kevin/settings/init-my-org.el
+++ users/kevin/settings/init-my-org.el
@@ -245,7 +245,7 @@
 ;; global Effort estimate values
 ;; global STYLE property values for completion
 (setq org-global-properties (quote (
-                                    ("Effort_ALL" . "0:15 0:30 0:45 1:00 2:00 3:00 4:00 5:00 6:00 0:00")
+                                    ;;("Effort_ALL" . "0:15 0:30 0:45 1:00 2:00 3:00 4:00 5:00 6:00 0:00")
                                     ("Status_ALL" . "Not-start In-Progress Delay Finished Cancled")
                                     ("ID_ALL" . "")
                                     ("STYLE_ALL" . "habit"))))
