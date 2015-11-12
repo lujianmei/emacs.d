ec32de26ff9419488a8b99884c3effc9fa36559c
lujianmei <anysky130@163.com>
2015-11-12 11:38:12 +0800

    changed taskjuggler

--- users/kevin/settings/init-taskjuggler.el
+++ users/kevin/settings/init-taskjuggler.el
@@ -11,31 +11,26 @@
 (setq org-taskjuggler-project-tag "project")
 (setq org-taskjuggler-resource-tag "resources")
 (setq org-taskjuggler-default-project-duration 365)
-(setq org-taskjuggler-default-global-properties "
-account cost "费用"
-account revenue "收入"
-balance cost revenue")
+(setq org-taskjuggler-default-global-properties "account cost \"费用\" account revenue \"收入\" balance cost revenue")
 
 
+(setq org-taskjuggler-default-reports '("include \"reports.tji\" \n include \"resources.tji\"  "))
 
-(setq org-taskjuggler-default-reports '("include "reports.tji""))
 
+;; (defun org-taskjuggler-generate-reports ()
+;;   (interactive)
+;;   (message "Generate Reports...")
+;;   (let* ((process-name "tj3")
+;;          (filename (expand-file-name
+;;                     (concat
+;;                      (file-name-sans-extension
+;;                       (file-name-nondirectory buffer-file-name))
+;;                      ".tjp")))
+;;          (command (concat process-name " " filename)))
+;;     (start-process-shell-command process-name nil command))
+;;   (message "Generate Reports...Done"))
 
-(defun org-taskjuggler-generate-reports ()
-  (interactive)
-
-  (message "Generate Reports...")
-  (let* ((process-name "tj3")
-         (filename (expand-file-name
-                    (concat
-                     (file-name-sans-extension
-                      (file-name-nondirectory buffer-file-name))
-                     ".tjp")))
-         (command (concat process-name " " filename)))
-    (start-process-shell-command process-name nil command))
-  (message "Generate Reports...Done"))
-
-(add-hook 'org-taskjuggler-final-hook 'org-taskjuggler-generate-reports)
+;;(add-hook 'org-taskjuggler-final-hook 'org-taskjuggler-generate-reports)
 
 (provide 'init-taskjuggler)
 ;; init-taskjuggler.el end here
